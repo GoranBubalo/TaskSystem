448a0443094db328305bbb97f81c79b2b0c945a7
GoranBubalo <you@example.com>
2025-05-22 21:47:59 +0200

    Added exceptions for user and refactored user exceptions

---
+++ User/app/exception/user_exceptions.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException, status
+
+class UserNotFoundException(HTTPException):
+    def __init__(self, detail: str = "User not found"):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+class UsernameAlreadyExistsException(HTTPException):
+    def __init__(self, detail: str = "Username already registered"):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+class EmailAlreadyExistsException(HTTPException):
+    def __init__(self, detail: str = "Email already registered"):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+class UsernameTakenException(HTTPException):
+    def __init__(self, detail: str = "Username already taken"):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
